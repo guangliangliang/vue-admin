0cf5d772badc5bcb08bfc2469902eb876920c65c
guangliangliang <17600498920@163.com>
2020-11-16 20:27:53 +0800

    feature:修改github page里面因不到css的问题

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,13 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+   mounted() {
+    /**
+     * 解决 css 引入图片在 github pages 无法获取的问题
+     */
+    const { NODE_ENV } = process.env;
+    document.documentElement.className = NODE_ENV;
+  }
 }
 </script>
