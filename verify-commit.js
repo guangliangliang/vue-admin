bd3be85330b808155a4c92d9146d40ef51e6fb8b
menglh18 <menglh18@chinaunicom.cn>
2021-01-06 17:25:05 +0800

    feat: update verify-commit.js

--- verify-commit.js
+++ verify-commit.js
@@ -3,8 +3,8 @@
  * @Version: 1.0
  * @Autor: gll
  * @Date: 2020-12-05 22:30:14
- * @LastEditors: gll
- * @LastEditTime: 2020-12-13 10:15:36
+ * @LastEditors: unicom
+ * @LastEditTime: 2021-01-06 17:24:38
  */
 const msgPath = process.env.HUSKY_GIT_PARAMS
 const msg = require('fs')
@@ -17,7 +17,7 @@ if (!commitRE.test(msg)) {
   console.log()
   console.error(`
         不合法的 commit 消息格式。
-        请查看 git commit 提交规范：https://github.com/woai3c/Front-end-articles/blob/master/git%20commit%20style.md
+        请查看 git commit 提交规范：https://github.com/guangliangliang/vue-admin/blob/master/gitCommitConfig.md
     `)
 
   process.exit(1)
