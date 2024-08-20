b534bd07b341d6d8c9ac4cd39f3479cbe70d6bae
guangliangliang <47070735+guangliangliang@users.noreply.github.com>
2024-08-20 14:52:54 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@ export default {
   name: 'App',
   mounted() {
     /**
-     * 解决 css 引入图片在 github pages 无法获取的问题
+     * 解决 css 引入图片在 github pages 无法获取的问题 dddddddddddddddddddddddddddddddddddd
      */
     const { NODE_ENV } = process.env
     document.documentElement.className = NODE_ENV
