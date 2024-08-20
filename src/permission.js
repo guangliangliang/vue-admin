184c724f138daccd09aa3eb175b321063bb18058
guangliangliang <47070735+guangliangliang@users.noreply.github.com>
2024-08-20 14:48:18 +0800

    Update permission.js

--- src/permission.js
+++ src/permission.js
@@ -5,7 +5,7 @@ import NProgress from 'nprogress' // progress bar
 import 'nprogress/nprogress.css' // progress bar style
 import { getToken } from '@/utils/auth' // get token from cookie
 import getPageTitle from '@/utils/get-page-title'
-
+// test  test  test  test 
 NProgress.configure({ showSpinner: false }) // NProgress Configuration
 
 const whiteList = ['/login'] // no redirect whitelist
